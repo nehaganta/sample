942efde7932bfa5610876c39556bb70e42c0832f
Neha Ganta <Neha Ganta@DESKTOP-0NRGV6V>
2019-06-25 11:52:07 +0530

    commit done

---
+++ sample/src/sample/github.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class github {
+	public static void main(String[] args) {
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println(args[0]+" technologies "+args[1]);
+	}
+
+}
