0ca3d370add3ce3c4849766d53ddc49a07d7cc06
Neha Ganta <Neha Ganta@DESKTOP-0NRGV6V>
2019-06-25 12:15:31 +0530

    commit done

---
+++ sample/src/sample/git3.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class git3 {
+	public static void main(String[] ar){
+		int num1=Integer.parseInt(ar[0]);
+		int num2=Integer.parseInt(ar[1]);
+		System.out.println(num1+num2);
+		}
+		}
