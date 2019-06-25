52ec23c3673b3236fc210bb67384c810454c953b
Neha Ganta <Neha Ganta@DESKTOP-0NRGV6V>
2019-06-25 19:18:10 +0530

    commit and done

---
+++ sample/src/sample/git7.java
@@ -0,0 +1,21 @@
+package sample;
+
+public class git7 {
+	public static void main(String[] ar)
+	{
+		char ch=13;
+		System.out.println(ch);
+		if(ch>=48 || ch<=57)
+		{
+		System.out.println("Digit");
+		}
+		else if(ch>=65 && ch<=90 || ch>=97 && ch<=122)
+		{
+		System.out.println("Alphabhet");
+		}
+		else
+		{
+		System.out.println("Special character");
+		}
+		}
+		}
