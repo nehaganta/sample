0be259c3d43353f08c558d3e03b344f76efe7e16
Neha Ganta <Neha Ganta@DESKTOP-0NRGV6V>
2019-06-25 19:09:37 +0530

    commit and done

---
+++ sample/src/sample/git6.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class git6 {
+	public static void main(String[] ar)
+	{
+	char a=ar[0].charAt(0);
+	char b=ar[1].charAt(0);
+	if(a>b)
+	{
+	System.out.print(ar[1]+","+ar[0]);
+	}
+	else
+	{
+	System.out.print(ar[0]+","+ar[1]);
+	}
+	}
+	
+
+}
