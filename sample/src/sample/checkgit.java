75886165265d7bf75e07e7c167487ff07600751b
Neha Ganta <Neha Ganta@DESKTOP-0NRGV6V>
2019-06-25 18:58:57 +0530

    commit and done

---
+++ sample/src/sample/checkgit.java
@@ -0,0 +1,20 @@
+package sample;
+
+public class checkgit
+{
+	public static void main(String[] ar){
+		int num=Integer.parseInt(ar[0]);
+		if(num>0)
+		{
+		System.out.println("Positive");
+		}
+		else if(num<0)
+		{
+		System.out.println("Negative");
+		}
+		else if(num==0)
+		{
+		System.out.println("Zero");
+		}
+		}
+		}
