277e5b4897f745edce43f42f566085e754df52db
Neha Ganta <Neha Ganta@DESKTOP-0NRGV6V>
2019-06-25 19:03:15 +0530

    commit and done

---
+++ sample/src/sample/git4.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class git4 {
+	public static void main(String[] ar){
+		int num=Integer.parseInt(ar[0]);
+	if (num%2==0)
+		System.out.println("number is even");
+	else if (num%2!=0)
+		System.out.println("number is odd");
+	}
+}
