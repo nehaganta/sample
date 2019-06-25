2810141cc296be6e4eb0c80fbbb4baf7235b1bc7
Neha Ganta <Neha Ganta@DESKTOP-0NRGV6V>
2019-06-25 19:06:59 +0530

    commit and done

---
+++ sample/src/sample/git5.java
@@ -0,0 +1,20 @@
+package sample;
+
+public class git5 {
+
+	public static void main(String[] ar){
+		if(ar.length==0)
+		{
+		System.out.println("No Values");
+		}
+		else
+		{
+		int i;
+		for(i=0;i<ar.length;i++)
+		{
+		System.out.print(ar[i]+",");
+		}
+		}
+		}
+		}
+
