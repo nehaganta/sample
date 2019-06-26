0a0bb9de9b7c7f20414b81b0f4d5fea8e0975499
Neha Ganta <Neha Ganta@DESKTOP-0NRGV6V>
2019-06-26 10:19:29 +0530

    commit done

---
+++ sample/src/sample/casechange.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class casechange {
+	public static void main(String[] ar)
+	{
+		//char ch='a';
+	char ch='N';
+	if(ch>=97 && ch<=122)
+	{
+	System.out.println(ch+"->"+Character.toUpperCase(ch));
+	}
+	else if(ch>=65 && ch<=90)
+	{
+	System.out.println(ch+"->"+Character.toLowerCase(ch));
+	}
+	}
+	}
+
+
