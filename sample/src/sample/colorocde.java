5de352821bf6bd53f17dcb8a8d09153adf1c2a9b
Neha Ganta <Neha Ganta@DESKTOP-0NRGV6V>
2019-06-26 10:27:27 +0530

    commit done

---
+++ sample/src/sample/colorocde.java
@@ -0,0 +1,38 @@
+package sample;
+
+public class colorocde {
+	public static void main(String[] ar)
+	{
+	char ch=ar[0].charAt(0);
+	switch(ch)
+	{
+	case 'R': System.out.println("R->Red"); 
+	break;
+	case 'B': System.out.println("B->Blue"); 
+	break;
+	case 'G': System.out.println("G->Green"); 
+	break;
+	case 'O': System.out.println("O->Orange"); 
+	break;
+	case 'Y': System.out.println("Y->Yellow"); 
+	break;
+	case 'W': System.out.println("W->White");
+	break;
+	case 'r': System.out.println("R->Red"); 
+	break;
+	case 'b': System.out.println("B->Blue"); 
+	break;
+	case 'g': System.out.println("G->Green");
+	break;
+	case 'o': System.out.println("O->Orange"); 
+	break;
+	case 'y': System.out.println("Y->Yellow"); 
+	break;
+	case 'w': System.out.println("W->White"); 
+	break;
+	default: System.out.println("Invalid Code"); 
+	break;
+	}
+	}
+	}
+
