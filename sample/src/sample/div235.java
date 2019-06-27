a6a30d6c80790e0ea77340d8229ddc58db7ec0d2
Neha Ganta <Neha Ganta@DESKTOP-0NRGV6V>
2019-06-27 18:56:30 +0530

    commit done

---
+++ sample/src/sample/div235.java
@@ -0,0 +1,17 @@
+package sample;
+public class div235 {
+	public static void main(String[] ar)
+	{
+	int i=1,count=0;
+	while(count<5)
+	{
+	if(i%2==0&&i%3==0&&i%5==0)
+	{
+	System.out.print(i+" ");
+	count++;
+	}
+	i++;
+	}
+	} 
+	}
+
